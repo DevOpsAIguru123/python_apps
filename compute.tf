39b097b34e4f4daff95f067c37ee7d8cc956e424
vinndevops <38119256+DevOpsAIguru123@users.noreply.github.com>
2024-12-17 23:50:16 -0600

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,4 +1,37 @@
-
+// Create a cluster policy for shared compute
+resource "databricks_cluster_policy" "shared_compute_policy" {
+  name = "databricks-sbx"
+  definition = jsonencode({
+    "spark_version": {
+      "type": "fixed",
+      "value": "15.4.x-scala2.12"
+    },
+    "node_type_id": {
+      "type": "fixed",
+      "value": "Standard_DS3_v2"
+    },
+    "driver_node_type_id": {
+      "type": "fixed",
+      "value": "Standard_DS3_v2"
+    },
+    "autotermination_minutes": {
+      "type": "fixed",
+      "value": 120
+    },
+    "autoscale.min_workers": {
+      "type": "fixed",
+      "value": 1
+    },
+    "autoscale.max_workers": {
+      "type": "fixed",
+      "value": 4
+    },
+    "custom_tags.PolicyName": {
+      "type": "fixed",
+      "value": "Shared Compute"
+    }
+  })
+}
 
 // Create a cluster first (if you haven't already)
 resource "databricks_cluster" "shared_cluster" {
@@ -6,7 +39,7 @@ resource "databricks_cluster" "shared_cluster" {
   spark_version          = "15.4.x-scala2.12"
   node_type_id           = "Standard_DS3_v2"
   driver_node_type_id    = "Standard_DS3_v2"
-  policy_id              = "Shared Compute"
+  policy_id              = databricks_cluster_policy.shared_compute_policy.id
   autotermination_minutes = 120
 
   autoscale {
